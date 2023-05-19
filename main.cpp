ddcb5e82b961a13ab1a6de711753ee045f663407
MathanCyient <133364181+MathanCyient@users.noreply.github.com>
2023-05-19 13:36:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,26 +8,21 @@
  int counta=acctime;
  int countb=brktime;
  do{
-	Car car1;
-	car1.initdata();
-	car1.display();
-	
-	while(counta!=0)
-	{
-	car1.accelerate();
-	counta = counta-1;
-	}
-	
-        while(countb!=0)
-	{
-	car1.brake();
-	countb =countb-1;
-	}
-
-	cout<<"Do you want to continue y/n?"<<endl;
-	cin>>select;
-	
+	 Car car1;
+	 car1.initdata();
+	 car1.display();
+	 while(counta!=0)
+	 {
+		 car1.accelerate();
+		 counta = counta-1;
+	 }
+	 while(countb!=0)
+	 {
+		 car1.brake();
+		 countb =countb-1;
+	 }
+	 cout<<"Do you want to continue y/n?"<<endl;
+	 cin>>select;
  }while(select!="n");
-	
-    return 0;
+	 return 0;
  }
